b0e1a3a202fbc6da6b0a28b11757894ee4eab77d
vanyingenzi <ingenzivany@gmail.com>
2024-05-23 21:46:49 +0200

    Set perf to 4M max size and running for max 500msecs

--- pre-post-scripts/run-perf.sh
+++ pre-post-scripts/run-perf.sh
@@ -4,38 +4,22 @@ IMPLEMENTATION=$(jq -r .implementation /tmp/interop-variables.json)
 ROLE=$(jq -r .role /tmp/interop-variables.json)
 LOG_DIR=$(jq -r .log_dir /tmp/interop-variables.json)
 
-if [[ -z "$LOG_DIR" ]]; then
-    echo "transfer size is empty"
-    exit 127
-fi
-
-if [[ -z "$IMPLEMENTATION" ]]; then
-    echo "Implementation is empty" > ${LOG_DIR}/strace.txt
-    exit 127
-fi
-
-if [[ -z "$ROLE" ]]; then
-    echo "role is empty" > ${LOG_DIR}/trace.txt
-    exit 127
-fi
-
-# Wait for a process entitle role
 while [ -z "$(pgrep -f $IMPLEMENTATION-$ROLE)" ]; do
-    sleep 0.1
+    sleep 0.2
 done
 
 pid=$(pgrep -f $IMPLEMENTATION-$ROLE)
 perf_pid=0
 
 if [[ $IMPLEMENTATION == "mcmpquic" ]]; then
-    sudo perf record -e cycles --cpu=0 -D 2000 -F 999 -g --call-graph dwarf -p $pid -o ${LOG_DIR}/perf.data &
+    sudo perf record -e cycles -D 1000 --cpu=0 -F 999 --max-size=4M --call-graph dwarf -p $pid -o ${LOG_DIR}/perf.data &
     perf_pid=$!
 else
-    sudo perf record -e cycles -D 2000 -F 999 -g --call-graph dwarf -p $pid -o ${LOG_DIR}/perf.data &
+    sudo perf record -e cycles -D 1000 -F 999 --max-size=4M --call-graph dwarf -p $pid -o ${LOG_DIR}/perf.data &
     perf_pid=$!
 fi
 
 if [[ $perf_pid -ne 0 ]]; then
-    sleep 1
+    sleep 0.5
     sudo kill -SIGINT $perf_pid
 fi
--- pre-post-scripts/stop-perf.sh
+++ pre-post-scripts/stop-perf.sh
@@ -2,20 +2,13 @@
 
 LOG_DIR=$(jq -r .log_dir /tmp/interop-variables.json)
 
-pkill -SIGINT -f 'perf record'
-
-sleep 3 # wait for perf to finish
-
-if [ -d ${LOG_DIR} ]; then
-    echo "perf data directory exists"
+if pgrep -f 'perf record' > /dev/null; then
+    echo "A perf record process is already running. Stopping it..."
+    pkill -SIGINT -f 'perf record'
+    sleep 1
 else
-    echo "perf data directory does not exist"
-    exit 127
+    echo "No existing perf record process found."
 fi
 
-touch ${LOG_DIR}/perf.json
-
-echo "perf data succeeded"
-sudo perf script -i ${LOG_DIR}/perf.data > ${LOG_DIR}/perf_record.txt
-sudo rm ${LOG_DIR}/perf.data
-
+sudo perf script -i "${LOG_DIR}/perf.data" > "${LOG_DIR}/perf_record.txt"
+sudo rm "${LOG_DIR}/perf.data"
